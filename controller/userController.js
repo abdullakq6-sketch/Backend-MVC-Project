6b0b42b136ab599a415f14e3cc1b8134e60967fa
Abdullah Khan <abdullakq6@gmail.com>
2026-07-27 10:39:25 +0500

    fix: correct typo in email registration error message

--- controller/userController.js
+++ controller/userController.js
@@ -6,7 +6,7 @@ export const registerUser = async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'Email already registereds' });
+      return res.status(400).json({ message: 'Email already registered' });
     }
 
     const newUser = new User({ name, email, password });
